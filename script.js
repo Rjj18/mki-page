97a532a8667eee9cd47058289949dc24f4b0c49f
Rjj18 <roger.psn12@gmail.com>
2023-02-02 14:39:56 -0300

    renderização dinamica

--- script.js
+++ script.js
@@ -13,3 +13,29 @@ function sumir () {
     hamburguer.style.display = "block"
     menuFechar.style.display = "none"
 }
+
+// Validação dos Fomulários
+
+let nomeForm2 = document.getElementById("nomeNews");
+let emailForm2 = document.getElementById("emailNews");
+let submitForm2 = document.getElementById("form2");
+
+let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+submitForm2.addEventListener("submit", function (event) {
+    if (nomeForm2.value == "" || emailForm2.value == "") {
+        event.preventDefault()
+        alert("digite algo")
+    }
+})
+
+submitForm2.addEventListener("submit", function (event) {
+    if (emailForm2.value.match(validRegex)) {
+    } else {
+        event.preventDefault()
+        alert("Digite um e-mail válido")
+    }
+})
+
+
+
