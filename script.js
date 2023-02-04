ddefe8ddc694fe554c4ac2e5520ebccee0c74eff
Rjj18 <roger.psn12@gmail.com>
2023-02-04 17:21:29 -0300

    update javascript

--- script.js
+++ script.js
@@ -22,24 +22,39 @@ const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0
 //Validação Formulário de Contato
 const submitForm1 = document.getElementById("form1");
 const erroForm1 = document.getElementsByClassName("valid1");
+const emailForm1 = document.getElementById("erroMail1");
 let msgErro1 = false;
 
 submitForm1.addEventListener("submit", function (e) {
     for (let i = 0; i < erroForm1.length; i++) {
         if (erroForm1[i].value == "") {
             e.preventDefault();
+            erroForm1[i].style.backgroundColor = "pink"
             msgErro1 = true;
         } else {
+            erroForm1[i].style.backgroundColor = "green"
             msgErro1 = false;
         }
     } 
     if (msgErro1 == true) {
         e.preventDefault();
-        alert("Digite algo")
+        alert("O formulário possui erros que não permite o envio")
     } else {
         msgErro1 = true;
     }
 })
+
+// emailForm1.addEventListener("blur", function (event) {
+//     if (emailForm1.value.match(validRegex)) {
+//         emailForm1.style.backgroundColor = "green";
+//         erroEmail.style.display = "none";
+//         msgErro2 = false;       
+//     } else {
+//         emailForm2.style.backgroundColor = "pink";
+//         erroEmail.style.display = "block";
+//         msgErro2 = true;     
+//     }
+// })
 
 
 
@@ -47,7 +62,7 @@ submitForm1.addEventListener("submit", function (e) {
 const nomeForm2 = document.getElementById("nomeNews");
 const emailForm2 = document.getElementById("emailNews");
 const submitForm2 = document.getElementById("form2");
-const erroEmail = document.getElementById("erroMail2");
+const erroEmail2 = document.getElementById("erroMail2");
 let msgErro2 = false;
 
 
@@ -66,11 +81,11 @@ submitForm2.addEventListener("submit", function (event) {
 emailForm2.addEventListener("blur", function (event) {
     if (emailForm2.value.match(validRegex)) {
         emailForm2.style.backgroundColor = "green";
-        erroEmail.style.display = "none";
+        erroEmail2.style.display = "none";
         msgErro2 = false;       
     } else {
         emailForm2.style.backgroundColor = "pink";
-        erroEmail.style.display = "block";
+        erroEmail2.style.display = "block";
         msgErro2 = true;     
     }
 })
